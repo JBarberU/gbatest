e3bec3da7baa51fdcfc63740c8392e52daf2bf31
John Barbero Unenge <john.barbero.unenge@gmail.com>
2014-04-04 02:14:28 +0200

    Add some test stuff

--- src/test.c
+++ src/test.c
@@ -1,27 +1,72 @@
 /* hello.c - Gameboy Advance Tutorial - Loirak Development */
 #define RGB16(r,g,b)  ((r)+(g<<5)+(b<<10)) 
 
-int main(void)
-{
-	char x,y;  
-	unsigned short* Screen = (unsigned short*)0x6000000; 
-	*(unsigned long*)0x4000000 = 0x403; // mode3, bg2 on 
+#define REG_VCOUNT *(volatile unsigned short*)0x04000006
+#define REG_KEYINPUT *(volatile int*)0x04000130
+#define REG_SCREEN ((unsigned short*)0x6000000)
 
+#define kSCREEN_WIDTH 240
+#define kSCREEN_HEIGHT 160 
+
+int m_x, m_y;
+
+void vid_vsync_pre(void)
+{
+    while(REG_VCOUNT >= 160);   // wait till VDraw
+}
+
+void vid_vsync_post(void)
+{
+    while(REG_VCOUNT < 160);    // wait till VBlank
+}
+
+void clear(const int color)
+{
+  char x,y;
 	// clear screen, and draw a blue back ground
 	for(x = 0; x<240;x++)   //loop through all x
 	{
 		for(y = 0; y<160; y++)  //loop through all y
 		{
-			Screen[x+y*240] = RGB16(0,0,31);  
+			REG_SCREEN[x + y * kSCREEN_WIDTH] = color;
 		}
 	}
+}
 
-	// draw a white HI on the background
-	for(x = 20; x<=60; x+=15)
-		for(y = 30; y<50; y++)  
-			Screen[x+y*240] = RGB16(31,31,31);  
-	for (x = 20; x < 35; x++)
-		Screen[x+40*240] = RGB16(31,31,31);  
+void square(int x, int y, int side, int color)
+{
+  char _x,_y;
 
-	while(1){}	//loop forever
+	for(_x = (x > 0 ? x : 0); _x < x + side; ++_x)
+		for(_y = (y > 0 ? y : 0); _y < y + side; ++_y)  
+			REG_SCREEN[_x + _y * kSCREEN_WIDTH] = color;
+}
+
+void handle_keys(void)
+{
+  if (REG_KEYINPUT & (1 << 4))
+    m_x -= 15;
+  if (REG_KEYINPUT & (1 << 5))
+    m_x += 15;
+  if (REG_KEYINPUT & (1 << 6))
+    m_y += 15;
+  if (REG_KEYINPUT & (1 << 7))
+    m_y -= 15;
+}
+int main(void)
+{
+  int bg = 0;
+  int fg = RGB16(31,31,31); 
+	*(unsigned long*)0x4000000 = 0x403; // set mode3, bg2 on 
+  m_x = m_y = 0;
+
+	while(1)
+  {
+    ++bg;
+    vid_vsync_post();
+    clear(bg);
+    square(m_x, m_y, 20, RGB16(31,0,31));
+    handle_keys();
+    vid_vsync_pre();
+  }	//loop forever
 }
