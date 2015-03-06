014ec400f3f8a36630ab0fcea2f0fdad64acc4e8
John Barbero Unenge <john.barbero.unenge@gmail.com>
2015-03-06 17:54:51 +0100

    Make bg gradually increase

--- src/test.c
+++ src/test.c
@@ -1,14 +1,21 @@
 /* hello.c - Gameboy Advance Tutorial - Loirak Development */
-#define RGB16(r,g,b)  ((r)+(g<<5)+(b<<10)) 
-
 #define REG_VCOUNT *(volatile unsigned short*)0x04000006
 #define REG_KEYINPUT *(volatile int*)0x04000130
 #define REG_SCREEN ((unsigned short*)0x6000000)
 
 #define kSCREEN_WIDTH 240
 #define kSCREEN_HEIGHT 160 
+#define RGB16(r,g,b) (r+(g<<5)+(b<<10))
 
 int m_x, m_y;
+
+int f_rgb(float fr, float fg, float fb)
+{
+  int r = (int)fr;
+  int g = (int)fg;
+  int b = (int)fb;
+  return RGB16(r,g,b);
+}
 
 void vid_vsync_pre(void)
 {
@@ -55,16 +62,21 @@ void handle_keys(void)
 }
 int main(void)
 {
-  int bg = 0;
+  float bgr = 0.f;
+  float bgg = 0.f;
+  float bgb = 0.f;
+
   int fg = RGB16(31,31,31); 
 	*(unsigned long*)0x4000000 = 0x403; // set mode3, bg2 on 
   m_x = m_y = 0;
 
 	while(1)
   {
-    ++bg;
+    bgr += 0.1f;
+    bgg += 0.2f;
+    bgb += 0.3f;
     vid_vsync_post();
-    clear(bg);
+    clear(f_rgb(bgr, bgg, bgb));
     square(m_x, m_y, 20, RGB16(31,0,31));
     handle_keys();
     vid_vsync_pre();
